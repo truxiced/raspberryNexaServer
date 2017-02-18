58a02dcb579f3e448b413782d82a49a2b9035d60
Gustav Granelli <gustav.granelli@gmail.com>
2017-02-18 15:38:41 +0000

    added on/off support for hardcoded id

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
+const exec = require('exec');
 
 app.listen(port, (err) => {  
   if (err) {
@@ -11,8 +11,28 @@ app.listen(port, (err) => {
   console.log(`server is listening on ${port}`)
 })
 
-app.get('/', (req, res) => {  
+app.get('/off', (req, res) => {  
     
+    res.send("It's dead!");
+exec('sudo ~/pihat/pihat --repeats=50 --id=1 --channel=0 --state=0',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});   
+})
+
+app.get('/on', (req, res) => {
+
     res.send("It's alive!");
-   
+exec('sudo ~/pihat/pihat --repeats=50 --id=1 --channel=0 --state=1',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+});
 })
