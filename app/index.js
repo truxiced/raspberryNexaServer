c2757a1327a1588d4771288793a0b8d4c7c39e53
Gustav Granelli <gustavgranelli@Gustavs-MacBook-Pro.local>
2017-02-18 14:04:22 +0100

    skeleton

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
+
+app.get('/', (req, res) => {  
+    
+    res.send("It's alive!");
+   
+})
